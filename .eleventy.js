c315f1f943782ac63dc800dc1651ef4a3a56804e
1gfelton <gtf@andrew.cmu.edu>
2025-09-07 10:44:57 -0400

    updated

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
     // Copy images to folder output
     eleventyConfig.addPassthroughCopy("src/img")
+    eleventyConfig.addPassthroughCopy("src/style.css")
 
     return {
         dir: {
